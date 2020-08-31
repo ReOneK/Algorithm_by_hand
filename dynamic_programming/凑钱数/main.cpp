ca68196daf71c25272338522f96da8d036f31a45
ReOneK <959512421@qq.com>
2020-08-31 09:52:01 +0800

    feat:结构修改

--- dynamic_programming/凑钱数/main.cpp
+++ dynamic_programming/凑钱数/main.cpp
@@ -16,7 +16,9 @@ int main(){
 
 int min_num_coins(vector<int>& coins,int amount){
 vector<int> dp(amount+1,amount+1);
+//base_line
 dp[0]=0;
+
 for(int i=0;i<dp.size();i++){
     for(int coin:coins){
         if(i-coin<0) continue;
