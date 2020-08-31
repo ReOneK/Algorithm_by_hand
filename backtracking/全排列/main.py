9c6f7a0ba6b649689577f7ff32ea59e7720648c0
ReOneK <959512421@qq.com>
2020-08-31 19:58:59 +0800

    permutation

--- backtracking/全排列/main.py
+++ backtracking/全排列/main.py
@@ -8,3 +8,27 @@ Date: 2020-08-31 09:57:18
 '''
 
 
+def permutations(arr, position, end):
+    """
+    调换法：对每一个位置分别固定，然后调换前后数据的位置得到全排列
+    """
+    if position == end:
+        print(arr)
+ 
+    else:
+        for index in range(position, end):
+ 
+            arr[index], arr[position] = arr[position], arr[index]
+            print(arr)
+            permutations(arr, position+1, end)
+            arr[index], arr[position] = arr[position], arr[index]
+            print(arr)
+ 
+ 
+# arr = ["a","b","c"]
+# permutations(arr, 0, len(arr))
+    
+    
+    
+
+    
